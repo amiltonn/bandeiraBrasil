6af900d457354df3792a0d201dccd5eca1d0eabb
amiltonn <43390764+amiltonn@users.noreply.github.com>
2018-09-18 18:58:12 -0300

    Update bandeiraPython.py

--- bandeiraPython.py
+++ bandeiraPython.py
@@ -4,3 +4,19 @@ aRectengle=Rectangle(Point(0,0),Point(500,400))
 aRectengle.setFill("green")
 aRectengle.draw(win)
 
+
+
+apolygon=Polygon(Point(50,200) ,Point(250,50) , Point(250,50) , Point(450,200), Point(250,350))
+apolygon.setFill("yellow")
+apolygon.draw(win)
+acircle=Circle(Point(250,200), 100)
+acircle.setFill("blue")
+acircle.draw(win)
+
+
+
+
+
+
+win.getMouse()
+win.close()
