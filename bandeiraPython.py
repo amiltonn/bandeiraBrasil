1da104be9897aa20ab14b594dbb95c45e63ba52b
amiltonn <43390764+amiltonn@users.noreply.github.com>
2018-09-18 21:21:15 -0300

    Update bandeiraPython.py

--- bandeiraPython.py
+++ bandeiraPython.py
@@ -13,18 +13,14 @@ acircle=Circle(Point(250,200), 100)
 acircle.setFill("blue")
 acircle.draw(win)
 
-for x in range(160,350):
-    y=0.0007868937*(x**2)+(-0.19290505676*x)+169.83498452
-    ponto=Point(x,y)
-    ponto.setOutline("white")
-    ponto.draw(win)
-
-for x in range(155,349):
-    y=0.00060461956*(x**2)+(-0.06672554347*x)+173.05546875
-    ponto=Point(x,y)
-    ponto.setOutline("white")
-    ponto.draw(win)
-
+w=0
+for z in range(0,20):
+    for x in range(159,353):
+        y=0.0007868937*(x**2)+(-0.19290505676*x)+169.83498452
+        ponto=Point(x-w,y+z)
+        ponto.setOutline("white")
+        ponto.draw(win)
+    w=w+0.3
 
 
 
